8f265e118fe5c6e4fb014bec7ad15af49273d2c0
DevHumbleChris <dev.humblechris254@gmail.com>
2021-06-29 13:08:16 +0300

    Commit --> Tue Jun 29 (2021) 1:08pm

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab, fas, far } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.use(BootstrapVue)
